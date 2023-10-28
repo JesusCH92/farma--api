e9e4a3c109742ec804f47abe24bacb1fc54dc0fe
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 21:30:33 +0200

    habilitamos la api para acumular puntos

---
+++ src/Punto/Infrastructure/Api/AcumuladorPuntoController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Punto\Infrastructure\Api;
+
+use App\Common\Infrastructure\Framework\SymfonyApiController;
+use App\Punto\ApplicationService\Acumular;
+use App\Punto\ApplicationService\DTO\AcumularRequest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class AcumuladorPuntoController extends SymfonyApiController
+{
+    public function __construct(private readonly Acumular $acumular)
+    {
+    }
+
+    #[Route('/acumulador-punto', name: 'app_acumulador_punto', methods: 'POST')]
+    public function acumuladorDePuntos(Request $request): JsonResponse
+    {
+        ($this->acumular)(
+            new AcumularRequest(
+                $request->request->getInt('farmacia_id'),
+                $request->request->getInt('cliente_id'),
+                $request->request->getInt('puntos')
+            )
+        );
+
+        return $this->response([], Response::HTTP_CREATED);
+    }
+}

0838705e2d900376fc179dc236495c02e182c1eb
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 20:31:07 +0200

    canjeamos el punto

--- src/Punto/Domain/Repository/PuntoRepository.php
+++ src/Punto/Domain/Repository/PuntoRepository.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Punto\Domain\Repository;
 
+use App\Cliente\Domain\Entity\Cliente;
+use App\Farmacia\Domain\Entity\Farmacia;
 use App\Punto\Domain\Entity\Punto;
 
 interface PuntoRepository
@@ -13,4 +15,6 @@ interface PuntoRepository
     public function findByIdByClienteSinCajear(Cliente $cliente, int $puntoId): ?Punto;
 
     public function save(Punto $punto): void;
+
+    public function canjearPuntos(Farmacia $farmacia, Punto ...$puntos): void;
 }

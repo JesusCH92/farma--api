52cd3aef3ba97ec152a87e37ba913b7d955fe03f
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 20:41:27 +0200

    definimos un la busqueda de los puntos no canjeados y otorgados de una farmacia en un periodo de tiempo

--- src/Farmacia/Domain/Repository/FarmaciaRepository.php
+++ src/Farmacia/Domain/Repository/FarmaciaRepository.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace App\Farmacia\Domain\Repository;
 
+use App\Farmacia\ApplicationService\DTO\ContadorPuntoNoCanjeadoRequest;
 use App\Farmacia\Domain\Entity\Farmacia;
+use App\Punto\Domain\Entity\Puntos;
 
 interface FarmaciaRepository
 {
     public function findById(int $farmaciaId): ?Farmacia;
+
+    public function puntosNoCanjeadosEnUnPeriodo(ContadorPuntoNoCanjeadoRequest $dto): Puntos;
 }

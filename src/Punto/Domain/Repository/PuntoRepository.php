dfbe2b0ed7796c60ee9efa14bc5ab31b2a52a1fc
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 20:04:35 +0200

    puerto para punto

---
+++ src/Punto/Domain/Repository/PuntoRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Punto\Domain\Repository;
+
+use App\Punto\Domain\Entity\Punto;
+
+interface PuntoRepository
+{
+    public function findById(int $puntoId): ?Punto;
+
+    public function save(Punto $punto): void;
+}

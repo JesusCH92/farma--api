bd0700cc2268c1646f6e4f13a9f53d41fb425fc1
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 21:32:12 +0200

    habilitamos la api para canjear puntos

---
+++ src/Punto/Infrastructure/Api/CanjeadorPuntoController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Punto\Infrastructure\Api;
+
+use App\Common\Infrastructure\Framework\SymfonyApiController;
+use App\Punto\ApplicationService\Canjear;
+use App\Punto\ApplicationService\DTO\CanjearRequest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class CanjeadorPuntoController extends SymfonyApiController
+{
+    public function __construct(private readonly Canjear $canjear)
+    {
+    }
+
+    #[Route('/canjeador-punto', name: 'app_canjeador_punto', methods: 'POST')]
+    public function canjeadorDePuntos(Request $request): JsonResponse
+    {
+        $puntoIds = array_map(fn($id) => (int)$id, explode(',', $request->request->get('punto_collection')));
+
+        ($this->canjear)(
+            new CanjearRequest(
+                $request->request->getInt('farmacia_id'),
+                $request->request->getInt('cliente_id'),
+                ...$puntoIds
+            )
+        );
+
+        return $this->response([]);
+    }
+}

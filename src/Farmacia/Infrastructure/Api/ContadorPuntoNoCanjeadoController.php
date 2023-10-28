555acb695f5a8ed492584cdf9a6ada9cf0810da4
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 21:48:12 +0200

    habilitamos la api para encontrar los puntos otorgados de una farmacia en un periodo

---
+++ src/Farmacia/Infrastructure/Api/ContadorPuntoNoCanjeadoController.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Farmacia\Infrastructure\Api;
+
+use App\Common\Infrastructure\Framework\SymfonyApiController;
+use App\Farmacia\ApplicationService\ContadorPuntoNoCanjeado;
+use App\Farmacia\ApplicationService\DTO\ContadorPuntoNoCanjeadoRequest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class ContadorPuntoNoCanjeadoController extends SymfonyApiController
+{
+    public function __construct(private readonly ContadorPuntoNoCanjeado $contadorPuntoNoCanjeado)
+    {
+    }
+
+    #[Route('/puntos-sin-canjear', name: 'app_contador_punto_no_canjeado', methods: 'GET')]
+    public function contadorDePuntosNoCanjeadosEnFarmacia(Request $request): Response
+    {
+        $response = ($this->contadorPuntoNoCanjeado)(
+            new ContadorPuntoNoCanjeadoRequest(
+                $request->query->getInt('farmacia_id'),
+                $this->convertDateOrFail($request->query->get('fecha_inicio')),
+                $this->convertDateOrFail($request->query->get('fecha_fin')),
+                null
+            )
+        );
+
+        return $this->response(['cantidad_puntos_acumulados' => $response->cantidadTotal()]);
+    }
+}

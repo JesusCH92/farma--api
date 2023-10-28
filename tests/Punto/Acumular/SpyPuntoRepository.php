70e85a4d4f6e1145a98548763efea701b0edf375
JesusCH92 <jcocaharo@hotmail.com>
2023-10-29 01:12:53 +0200

    implementacion spy

---
+++ tests/Punto/Acumular/SpyPuntoRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Tests\Punto\Acumular;
+
+use App\Cliente\Domain\Entity\Cliente;
+use App\Farmacia\Domain\Entity\Farmacia;
+use App\Punto\Domain\Entity\Punto;
+use App\Punto\Domain\Entity\Puntos;
+use App\Punto\Domain\Repository\PuntoRepository;
+
+class SpyPuntoRepository implements PuntoRepository
+{
+    private $validateWasCalled = false;
+
+    public function findByIdByClienteSinCajear(Cliente $cliente, int $puntoId): ?Punto
+    {
+        return null;
+    }
+
+    public function save(Punto $punto): void
+    {
+        $this->validateWasCalled = true;
+    }
+
+    public function canjearPuntos(Farmacia $farmacia, Punto ...$puntos): void
+    {
+    }
+
+    public function findAllPuntosSinCanjearByCliente(Cliente $cliente): Puntos
+    {
+        return new Puntos([]);
+    }
+
+    public function verify(): bool
+    {
+        return $this->validateWasCalled;
+    }
+}

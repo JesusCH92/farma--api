71d5b1c75e1c21bf71f4dd6633cf698b306ccdfd
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 21:41:19 +0200

    creamos una funcion para validar el primitivo datetime

--- src/Common/Infrastructure/Framework/SymfonyApiController.php
+++ src/Common/Infrastructure/Framework/SymfonyApiController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Common\Infrastructure\Framework;
 
+use App\Common\Domain\Constant\Date;
+use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,5 +17,20 @@ abstract class SymfonyApiController extends AbstractController
         $statusResponse = $statusResponse ?? Response::HTTP_OK;
 
         return new JsonResponse(['data' => $data], $statusResponse);
+    }
+
+    final protected function convertDateOrFail(
+        ?string $dateString,
+        string $format = Date::FORMAT_STANDAR
+    ): DateTimeImmutable {
+        $date = $this->convertStringToDate($dateString, $format);
+
+        if (null === $date) {
+            throw new \InvalidArgumentException(
+                sprintf('La fecha <%s>, no tiene el formato <%s>', $dateString, $format)
+            );
+        }
+
+        return $date;
     }
 }

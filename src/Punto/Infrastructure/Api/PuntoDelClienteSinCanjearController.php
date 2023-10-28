5eb46a812857886bd041272ca210b37580ba1cee
JesusCH92 <jcocaharo@hotmail.com>
2023-10-28 21:56:56 +0200

    habilitamos la api para encontrar los puntos otorgados de una farmacia en un periodo para un cliente

---
+++ src/Punto/Infrastructure/Api/PuntoDelClienteSinCanjearController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Punto\Infrastructure\Api;
+
+use App\Common\Infrastructure\Framework\SymfonyApiController;
+use App\Punto\ApplicationService\Find\PuntosNoCanjeadosPorCliente;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class PuntoDelClienteSinCanjearController extends SymfonyApiController
+{
+    public function __construct(private readonly PuntosNoCanjeadosPorCliente $puntosNoCanjeadosPorCliente)
+    {
+    }
+
+    #[Route('/cliente-puntos-disponibles', name: 'app_punto_no_canjeado_por_cliente', methods: 'GET')]
+    public function puntosNoCanjeadosDelCliente(Request $request): JsonResponse
+    {
+        $response = ($this->puntosNoCanjeadosPorCliente)($request->query->getInt('cliente_id'));
+
+        return $this->response(['puntos' => $response->cantidadTotal()]);
+    }
+}

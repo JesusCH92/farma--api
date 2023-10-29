bbbbd0ab8f404c54b870a768beab001b3612a588
JesusCH92 <jcocaharo@hotmail.com>
2023-10-29 13:52:04 +0100

    spy para revisar el comportamiento del servicio de canjear

---
+++ tests/Punto/Canjear/SpyPuntoRepository.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Punto\Canjear;
+
+use App\Cliente\Domain\Entity\Cliente;
+use App\Farmacia\Domain\Entity\Farmacia;
+use App\Punto\Domain\Entity\Punto;
+use App\Punto\Domain\Entity\Puntos;
+use App\Punto\Domain\Repository\PuntoRepository;
+use App\Tarjeta\Domain\Entity\Tarjeta;
+
+class SpyPuntoRepository implements PuntoRepository
+{
+    private bool $validateWasCalled = false;
+
+    public function findByIdByClienteSinCajear(Cliente $cliente, int $puntoId): ?Punto
+    {
+        return new Punto(new Cliente(new Tarjeta(100.00)), new Farmacia('a'), 100);
+    }
+
+    public function save(Punto $punto): void
+    {
+    }
+
+    public function canjearPuntos(Farmacia $farmacia, Punto ...$puntos): void
+    {
+        $this->validateWasCalled = true;
+    }
+
+    public function findAllPuntosSinCanjearByCliente(Cliente $cliente): Puntos
+    {
+        return new Puntos([]);
+    }
+
+    public function verify(): bool
+    {
+        return $this->validateWasCalled;
+    }
+}
